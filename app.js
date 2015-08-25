aa49f135b62702b0232c1b7f4b77f9614bd77c2b
Michael <Michael.ginalick@gmail.com>
2015-08-24 22:47:38 -0500

    this tutorial is fucking aweful

--- app.js
+++ app.js
@@ -12,23 +12,6 @@ app.use(expressSession({secret: 'mySecretKey'}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
-passport.serializeUser(function(user, done) {
-  done(null, user._id);
-});
- 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
-
-
-
-
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
